a8b151753fcf6dc39912a7391cbf3a2eabaf613e
Firman <firwijaya@yahoo.com>
2025-03-23 11:38:08 +0700

    Remove junit deps and use Spring boot managed junit version

--- src/test/java/com/firman/dirmon/monitor/DirMonitorServiceTest.java
+++ src/test/java/com/firman/dirmon/monitor/DirMonitorServiceTest.java
@@ -1,10 +1,8 @@
-package com.firman.dirmon.ingest;
+package com.firman.dirmon.monitor;
 
-import com.firman.dirmon.monitor.DirMonitorService;
-import com.firman.dirmon.monitor.DirectoryMonitor;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
+import com.firman.dirmon.ingest.IngestFile;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import org.mockito.Mockito;
 import org.springframework.util.FileCopyUtils;
 
@@ -16,17 +14,14 @@ import static org.mockito.Mockito.verify;
 
 public class DirMonitorServiceTest {
 
-    @Rule
-    public TemporaryFolder tmp = new TemporaryFolder();
-
     @Test
-    public void testProcessEvents() throws Exception {
-        File watchedDir = tmp.newFolder();
+    public void testProcessEvents(@TempDir File watchedDir) throws Exception {
         IngestFile ingestFile = Mockito.mock(IngestFile.class);
         DirectoryMonitor monitor = new DirMonitorService(ingestFile, watchedDir.getAbsolutePath());
         Thread dirMonitorThread = new Thread(monitor::processEvents);
         dirMonitorThread.start();
-        File newFile = tmp.newFile("newFile");
+        File newFile = Paths.get(watchedDir.getAbsolutePath(), "newFile").toFile();
+        newFile.createNewFile();
         FileCopyUtils.copy(newFile, Paths.get(watchedDir.getAbsolutePath(), "newFile").toFile());
         // The tricky part is sometimes it takes longer for the Java app to get filesystem notification
         // The day the event takes longer than 10 sec to register is the day this test fails ):
@@ -36,20 +31,18 @@ public class DirMonitorServiceTest {
     }
 
     @Test
-    public void testProcessEventsInterrupted() throws Exception {
-        File watchedDir = tmp.newFolder();
+    public void testProcessEventsInterrupted(@TempDir File watchedDir) throws Exception {
         IngestFile ingestFile = Mockito.mock(IngestFile.class);
         DirMonitorService dirMonitor = new DirMonitorService(ingestFile, watchedDir.getAbsolutePath());
         Thread dirMonitorThread = new Thread(dirMonitor::processEvents);
         dirMonitorThread.start();
         // Calling interrupt on the thread will result in processEvents() to return
         dirMonitorThread.interrupt();
-        File newFile = tmp.newFile("newFile");
+        File newFile = Paths.get(watchedDir.getAbsolutePath(), "newFile").toFile();
+        newFile.createNewFile();
         FileCopyUtils.copy(newFile, Paths.get(watchedDir.getAbsolutePath(), "newFile").toFile());
         Thread.sleep(10000);
         // No event should be registered
         verify(ingestFile, times(0)).ingestFile(Paths.get(watchedDir.getAbsolutePath(), "newFile").toString());
     }
-
-
 }
